f7ab859da84e26bad7dcd40f54b2c891b7af9a8c
Jonas Subel <jsubel@students.42wolfsburg.de>
2022-11-08 15:09:44 +0100

    ready for eval

--- services/bonus/vsftpd/tools/start.sh
+++ services/bonus/vsftpd/tools/start.sh
@@ -4,14 +4,14 @@ if [ ! -f "/etc/vsftpd.conf.backup" ]; then
 
 mkdir -p /var/run/vsftpds/empty
 
-cerp /etc/vsftpd.conf /etc/vsftpd.conf.backup
+cp /etc/vsftpd.conf /etc/vsftpd.conf.backup
 mv /var/www/vsftpd.conf /etc/vsftpd.conf
 
 adduser $FTP_USR --disabled-password
-echo "$FTP_USR:$FTP_PWD" | /usr/sbin/chpasswd # &> /dev/null
-chown -R $FTP_USR:$FTP_PWD /var/www/html/wordpress
+echo "$FTP_USR:$FTP_PWD" | /usr/sbin/chpasswd &> /dev/null
+chown -R $FTP_USR:$FTP_USR /var/www/html/wordpress
 
-#echo $FTP_USR | tee -a /etc/vsftpd.userlist &> /dev/null
+echo $FTP_USR | tee -a /etc/vsftpd.userlist &> /dev/null
 
 fi
 
