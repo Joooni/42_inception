7b470dd01dec5972b1a250789eb0cfbbddb9304e
Jonas Subel <jsubel@students.42wolfsburg.de>
2022-12-13 12:00:22 +0100

    removed FTP service as it broke wordpress somewhat

--- services/bonus/redis/tools/start.sh
+++ services/bonus/redis/tools/start.sh
@@ -1,5 +1,9 @@
 cp /etc/redis/redis.conf /etc/redis/redis.conf.bak
 
+# Comment out the bind to certain address in redis conf
+# uncomment and set maxmemory to be used by redis
+# maxmemory policy: what happens when the memory is full and a new key comes in
+#	in this case, evict (remove) the least recently used (LRU) key from keyset
 sed -i "s|bind 127.0.0.1|#bind 127.0.0.1|g"  /etc/redis.conf
 sed -i "s|# maxmemory <bytes>|maxmemory 20mb|g"  /etc/redis.conf
 echo "maxmemory-policy allkeys-lru" >> /etc/redis.conf
