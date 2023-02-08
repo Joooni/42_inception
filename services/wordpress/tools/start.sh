2e574805c4c720e471e79d8120ab26e43661f081
Jonas Subel <jsubel@students.42wolfsburg.de>
2023-02-08 11:00:03 +0100

    revert

--- services/wordpress/tools/start.sh
+++ services/wordpress/tools/start.sh
@@ -81,12 +81,12 @@ wp user create \
 wp theme install inspiro --activate --allow-root
 
 # enable redis cache
-    # sed -i "40i define( 'WP_REDIS_HOST', '$REDIS_HOST' );"      wp-config.php
-    # sed -i "41i define( 'WP_REDIS_PORT', 6379 );"               wp-config.php
+    sed -i "40i define( 'WP_REDIS_HOST', '$REDIS_HOST' );"      wp-config.php
+    sed -i "41i define( 'WP_REDIS_PORT', 6379 );"               wp-config.php
     # sed -i "42i define( 'WP_REDIS_PASSWORD', '$REDIS_PWD' );"   wp-config.php
-    # sed -i "42i define( 'WP_REDIS_TIMEOUT', 1 );"               wp-config.php
-    # sed -i "43i define( 'WP_REDIS_READ_TIMEOUT', 1 );"          wp-config.php
-    # sed -i "44i define( 'WP_REDIS_DATABASE', 0 );\n"            wp-config.php
+    sed -i "42i define( 'WP_REDIS_TIMEOUT', 1 );"               wp-config.php
+    sed -i "43i define( 'WP_REDIS_READ_TIMEOUT', 1 );"          wp-config.php
+    sed -i "44i define( 'WP_REDIS_DATABASE', 0 );\n"            wp-config.php
 
 wp plugin install redis-cache --activate --allow-root
 wp plugin update --all --allow-root
@@ -96,7 +96,7 @@ else
 echo "Wordpress: is already set up!"
 fi
 
-#wp redis enable --allow-root
+wp redis enable --allow-root
 
 echo "Wordpress started on :9000"
 
